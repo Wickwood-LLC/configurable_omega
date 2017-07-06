a8327cbea5fbf0bb7b32577b230e72105399f70f
palagpatski <palagpatski@gmail.com>
2017-07-06 23:06:20 +0800

    Added preview JS

--- color/preview.js
+++ color/preview.js
@@ -22,10 +22,10 @@
       $('#preview-footer', form).css('backgroundColor', $('#palette input[name="palette[footer]"]', form).val());
  
       // CSS3 Gradients.
-      var gradient_start = $('#palette input[name="palette[header_top]"]', form).val();
-      var gradient_end = $('#palette input[name="palette[header_bottom]"]', form).val();
+      // var gradient_start = $('#palette input[name="palette[header_top]"]', form).val();
+      // var gradient_end = $('#palette input[name="palette[header_bottom]"]', form).val();
  
-      $('#preview #preview-header', form).attr('style', "background-color: " + gradient_start + "; background-image: -webkit-gradient(linear, 0% 0%, 0% 100%, from(" + gradient_start + "), to(" + gradient_end + ")); background-image: -moz-linear-gradient(-90deg, " + gradient_start + ", " + gradient_end + ");");
+      // $('#preview #preview-header', form).attr('style', "background-color: " + gradient_start + "; background-image: -webkit-gradient(linear, 0% 0%, 0% 100%, from(" + gradient_start + "), to(" + gradient_end + ")); background-image: -moz-linear-gradient(-90deg, " + gradient_start + ", " + gradient_end + ");");
     }
   };
 })(jQuery);
