f123e14219fc9b2a5833d17d38392c0ca70c479c
palagpatski <palagpatski@gmail.com>
2017-07-06 19:20:24 +0800

    Added preview JS

---
+++ color/preview.js
@@ -0,0 +1,31 @@
+// Handle the color changes and update the preview window.
+(function ($) {
+  Drupal.color = {
+    logoChanged: false,
+    callback: function(context, settings, form, farb, height, width) {
+      // Background
+      $('#preview', form).css('backgroundColor', $('#palette input[name="palette[bg]"]', form).val());
+
+      // Header
+      $('#preview-header', form).css('backgroundColor', $('#palette input[name="palette[header]"]', form).val());
+ 
+      // Text
+      $('#preview #preview-main h2, #preview .preview-content', form).css('color', $('#palette input[name="palette[text]"]', form).val());
+ 
+      // Links
+      $('#preview #preview-content a', form).css('color', $('#palette input[name="palette[link]"]', form).val());
+ 
+      // Titles
+      $('#preview h1, #preview h2', form).css('color', $('#palette input[name="palette[titles]"]', form).val());
+ 
+      // Footer
+      $('#preview-footer', form).css('backgroundColor', $('#palette input[name="palette[footer]"]', form).val());
+ 
+      // CSS3 Gradients.
+      var gradient_start = $('#palette input[name="palette[header_top]"]', form).val();
+      var gradient_end = $('#palette input[name="palette[header_bottom]"]', form).val();
+ 
+      $('#preview #preview-header', form).attr('style', "background-color: " + gradient_start + "; background-image: -webkit-gradient(linear, 0% 0%, 0% 100%, from(" + gradient_start + "), to(" + gradient_end + ")); background-image: -moz-linear-gradient(-90deg, " + gradient_start + ", " + gradient_end + ");");
+    }
+  };
+})(jQuery);
